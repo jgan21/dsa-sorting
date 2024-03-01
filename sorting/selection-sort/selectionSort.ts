e33649d35b7c77115735658a3d92db0037df5e86
Charles Chitty <charlescchitty@gmail.com>
2024-03-01 12:35:05 -0800

    Implementned selectionsort

--- sorting/selection-sort/selectionSort.ts
+++ sorting/selection-sort/selectionSort.ts
@@ -1,7 +1,19 @@
 /** Sort in-place and return sorted array. */
 
 function selectionSort(nums: number[]): number[] {
+  // let smallestElement = Math.min(...nums);
+  let smallestElementIndex = 0
+  for (let i = 0; i < nums.length; i++){
+    if (nums[i] < nums[smallestElementIndex]){
+      smallestElementIndex = i
+    }
+
+  }
   return nums;
 }
+
+let oldValue = nums[smallestElement]
+nums[smallestElement] = nums[i]
+nums[i] = oldValue
 
 export { selectionSort };
