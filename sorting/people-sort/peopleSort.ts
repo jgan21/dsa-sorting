8443f20c3cfb2d2826c128f6462b3f94ff145dc9
Jia Rong Gan <jiaronggan21@gmail.com>
2024-03-01 14:23:22 -0600

    Completed peopleSort and caseInsensitive

--- sorting/people-sort/peopleSort.ts
+++ sorting/people-sort/peopleSort.ts
@@ -1,9 +1,30 @@
-type tPerson = {fname: string, lname: string};
+type tPerson = { fname: string, lname: string; };
 
 /** Sort array of persons in place (lname > fname). Returns array. */
 
 function peopleSort(arr: tPerson[]): tPerson[] {
-  return [];
+  const sorted = arr.sort((a: tPerson, b: tPerson): number => {
+    if (a.lname < b.lname) {
+      return -1;
+    } else if (a.lname > b.lname) {
+      return 1;
+    } else {
+      if (a.fname < b.fname) {
+        return -1;
+      }
+      else if (a.fname > b.fname) {
+        return 1;
+      }
+      else {
+        return 0;
+      }
+    }
+  }
+);
+  return sorted;
 }
 
 export { peopleSort };
+// c before j -> c less j
+// c = c -> 0
+// j after c = j more c
